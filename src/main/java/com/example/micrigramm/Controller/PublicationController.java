8ddfc565345613f8bd2a20ecdc8f91bf0f5c9ebf
AydaykaMirbekova <aidaika.mirbekova@gmail.com>
2022-04-01 12:06:00 +0600

    Lesson 53 (commit 2) add method to add publication with image and description and delete publication with comment

---
+++ src/main/java/com/example/micrigramm/Controller/PublicationController.java
@@ -0,0 +1,38 @@
+package com.example.micrigramm.Controller;
+
+import com.example.micrigramm.DTO.PublicationDTO;
+import com.example.micrigramm.Service.PublicationService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+
+@RestController
+@RequestMapping("/publications")
+public class PublicationController {
+    private final PublicationService publicationService;
+
+    public PublicationController(PublicationService publicationService) {
+        this.publicationService = publicationService;
+    }
+
+    @PostMapping("/newPublic")
+    public ResponseEntity<String> addPublic(@RequestParam("file") MultipartFile file, String email , String description) throws Exception {
+       publicationService.addPublic(file,email,description);
+        return ResponseEntity.ok().build();
+
+    }
+    @GetMapping("/{publicationId}")
+    public PublicationDTO getPublication(@PathVariable Long publicationId) {
+        return publicationService.findPublication(publicationId);
+    }
+
+    @DeleteMapping("/delete/{publicId}")
+    public ResponseEntity<String> deletePublic(@PathVariable Long publicId,Long usarId){
+        if (publicationService.deletePublication(publicId,usarId)) {
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+}
---
+++ src/main/java/com/example/micrigramm/Service/PublicationService.java
@@ -0,0 +1,58 @@
+package com.example.micrigramm.Service;
+
+import com.example.micrigramm.DTO.PublicationDTO;
+import com.example.micrigramm.Entity.Publication;
+import com.example.micrigramm.Exception.ResourceNotFoundException;
+import com.example.micrigramm.Repository.CommentRepository;
+import com.example.micrigramm.Repository.PublicationRepository;
+import com.example.micrigramm.Repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+@Transactional
+@RequiredArgsConstructor
+@Service
+public class PublicationService {
+    private final PublicationRepository publicationRepository;
+    private final UserRepository userRepository;
+    private final CommentRepository commentRepository;
+
+    public void addPublic(MultipartFile file, String email, String description) throws Exception {
+        byte[] data = new byte[0];
+        try {
+            data = file.getBytes();
+        } catch (
+                IOException e) {
+            e.printStackTrace();
+        }
+        var user = userRepository.findByEmailContainsIgnoringCase(email);
+        var publication = Publication.builder()
+                .author(user)
+                .image(data)
+                .description(description)
+                .dateAdded(LocalDateTime.now())
+                .build();
+
+        if (user == null) {
+            throw new Exception("User not found!");
+        }
+        publicationRepository.save(publication);
+    }
+
+    public PublicationDTO findPublication(Long publicationId) {
+        var publication = publicationRepository.findPublicationById(publicationId);
+        if (publication == null) {
+       throw new ResourceNotFoundException("Can't find movie with the ID: " + publicationId);}
+        return PublicationDTO.from(publication);
+    }
+
+    public boolean deletePublication(Long publicId, Long autorId) {
+        commentRepository.deleteCommentsByPublicationId(publicId);
+        publicationRepository.deletePublicationByIdAndAuthorId(publicId, autorId);
+        return true;
+    }
+}
